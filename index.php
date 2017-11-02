3adda4bcc29570c7d0d13cf1746df31daee08546
Anne-Marije <napannemarije@gmail.com>
2017-11-02 16:02:24 +0100

    klikkleur

--- index.php
+++ index.php
@@ -1,79 +1,86 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
+        
+        <script>
+            
+            function kleur(){
+                alert("test");
+                    document.getElementById('B7').innerHTML = 'piet';
+                    document.getElementById('B7').style.backgroundColor = 'orange';
+            }
+            
+         </script>
         <style>
             table , tr, td {
                 border:2px solid black;
             }
+            button {border-radius: 50%;}
+            
+           
         </style>
         
         
     </head>
     <body>
+        <button>ajdflsajdf</button>
+        <input type="button" value="sdlkfjasl" />
         <table>
             <tr>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
+                <?php
+                for($x =0;$x<6;$x++){
+                    echo '<td><button onclick="kleur()" type="button" id="B7">_</button></td>';
+
+                }
+                
+                ?>
             </tr>
             <tr>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
+                <?php
+                for($x =0;$x<6;$x++){
+                    echo '<td><button type="button" id="B7">_</button></td>';
+
+                }
+                
+                ?>
+                
             </tr>
             <tr>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
+               <?php
+                for($x =0;$x<6;$x++){
+                    echo '<td><button type="button" id="B7">_</button></td>';
+
+                }
+                
+                ?>
             </tr>
             <tr>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
+                <?php
+                for($x =0;$x<6;$x++){
+                    echo '<td><button type="button" id="B7">_</button></td>';
+
+                }
+                
+                ?>
             </tr>
             <tr>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
+                <?php
+                for($x =0;$x<6;$x++){
+                    echo '<td><button type="button" id="B7">_</button></td>';
+
+                }
+                
+                ?>
             </tr>
             <tr>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
-                <td><button type="button">o</button></td>
+                <?php
+                for($x =0;$x<6;$x++){
+                    echo '<td><button type="button" id="B7">_</button></td>';
+
+                }
+                
+                ?>
             </tr>
-            
         </table>
-        <?php
-        
-        ?>
     </body>
 </html>
