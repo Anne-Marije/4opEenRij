02d30d954e59f1242fdef8c524965984bc39e82f
Anne-Marije <napannemarije@gmail.com>
2017-11-02 14:28:47 +0100

    bord

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <style>
+            table , tr, td {
+                border:2px solid black;
+            }
+        </style>
+        
+        
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td>o</td><td>o</td><td>o</td><td>o</td><td>o</td><td>o</td><td>o</td>
+            </tr>
+            <tr>
+                <td>o</td><td>o</td><td>o</td><td>o</td><td>o</td><td>o</td><td>o</td>
+            </tr>
+            <tr>
+                <td>o</td><td>o</td><td>o</td><td>o</td><td>o</td><td>o</td><td>o</td>
+            </tr>
+            <tr>
+                <td>o</td><td>o</td><td>o</td><td>o</td><td>o</td><td>o</td><td>o</td>
+            </tr>
+            <tr>
+                <td>o</td><td>o</td><td>o</td><td>o</td><td>o</td><td>o</td><td>o</td>
+            </tr>
+            <tr>
+                <td>o</td><td>o</td><td>o</td><td>o</td><td>o</td><td>o</td><td>o</td>
+            </tr>
+            
+        </table>
+        <?php
+        
+        ?>
+    </body>
+</html>
